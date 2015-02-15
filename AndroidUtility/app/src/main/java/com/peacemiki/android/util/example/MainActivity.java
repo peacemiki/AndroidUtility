6ed4ac8a2ef537120130419432d06df3542c327b
Sung-shin Park <sinschool@gmail.com>
2015-02-15 23:58:22 +0900

    resolve conflict

--- AndroidUtility/app/src/main/java/com/peacemiki/android/util/example/MainActivity.java
+++ AndroidUtility/app/src/main/java/com/peacemiki/android/util/example/MainActivity.java
@@ -1,10 +1,7 @@
 package com.peacemiki.android.util.example;
 
-<<<<<<< HEAD
 import android.app.Activity;
-=======
 import android.content.Context;
->>>>>>> feature/ViewHolder
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
